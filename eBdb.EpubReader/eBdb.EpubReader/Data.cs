37a76cc917a140769ed277f908f8b4d63d35745c
ljyok <xhflektm117@gmail.com>
2017-02-21 16:21:41 +0900

    epub reader dll file source code commit

---
+++ eBdb.EpubReader/eBdb.EpubReader/Data.cs
@@ -0,0 +1,14 @@
+﻿namespace eBdb.EpubReader
+{
+    public class DateData
+    {
+        public string Type { get; private set; }
+        public string Date { get; private set; }
+
+        public DateData(string type, string date)
+        {
+            Type = type;
+            Date = date;
+        }
+    }
+}
